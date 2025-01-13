c2e2c5177fedefcd63d9ab6df8168e7483fd9c96
DaltonBritton <157634547+DaltonBritton@users.noreply.github.com>
2025-01-13 13:36:54 -0700

    Updated Database

---
+++ Farmetta/Farmetta/Migrations/20250113035326_AddMoonrakerInstance.cs
@@ -0,0 +1,35 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Farmetta.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddMoonrakerInstance : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "MoonrakerInstances",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "INTEGER", nullable: false)
+                        .Annotation("Sqlite:Autoincrement", true),
+                    Name = table.Column<string>(type: "TEXT", nullable: false),
+                    Uri = table.Column<string>(type: "TEXT", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_MoonrakerInstances", x => x.Id);
+                });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "MoonrakerInstances");
+        }
+    }
+}
